e628880b67652b520e1a6d87f9bbecdd3506984c
oliver.stuckert <Oliver.Stuckert@powwow.de>
2023-02-13 15:32:20 +0100

    +WebMvcTest +SpringBootTest

---
+++ catalog-service/src/test/java/com/example/catalogservice/BookControllerMvcTests.java
@@ -0,0 +1,30 @@
+package com.example.catalogservice;
+
+import com.example.catalogservice.controller.BookController;
+import com.example.catalogservice.exception.BookNotFoundException;
+import com.example.catalogservice.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTests {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception {
+
+        String isbn = "73737313940";
+        given(bookService.viewBookDetail(isbn)).willThrow(BookNotFoundException.class);
+        mockMvc.perform(get("/books/" + isbn)).andExpect(status().isNotFound());
+    }
+}
--- catalog-service/src/test/java/com/example/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/example/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.catalogservice;
 
+import com.example.catalogservice.entity.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void whenPostRequestThenBookCreated() {
+        var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+
+        webTestClient
+                .post()                                     //Prepare an HTTP POST request
+                .uri("/books")                          //Sends the request to the "/books" endpoint
+                .bodyValue(expectedBook)                    //Adds the book in the request body
+                .exchange()                                 //Sends the request
+                .expectStatus().isCreated()                 //Verifies that the HTTP response has status “201 Created”
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();     //Verifies that the HTTP response has a non-null body
+                    assertThat(actualBook.isbn())
+                            .isEqualTo(expectedBook.isbn());//Verifies that the created object is as expected
+                });
+
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
